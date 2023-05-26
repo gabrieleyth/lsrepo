4032d4a448809e46ba1e2204597f323226d3bd64
gabrieleyth <gabriel.leyth@outlook.com>
2023-05-26 07:47:48 +0000

    simple age program

---
+++ age.js
@@ -0,0 +1,11 @@
+let age = 10;
+let age10 = age + 10
+let age20 = age + 20
+let age30 = age + 30
+let age40 = age + 40
+
+console.log('You are ' + age + ' years old.');
+console.log('In 10 years youll be ' + age10 + ' years old.');
+console.log('In 20 years youll be ' + age20 + ' years old.');
+console.log('In 30 years youll be ' + age30 + ' years old.');
+console.log('In 40 years youll be ' + age40 + ' years old.');
