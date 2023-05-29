0bf78998cb771c9e73cd9206fccf1faf27ed6136
gabrieleyth <gabriel.leyth@outlook.com>
2023-05-29 07:16:31 +0000

    adding programs that check whether said array has even numbers and returns them

---
+++ even.js
@@ -0,0 +1,4 @@
+let arr = [1, 3, 6, 11, 4, 2,
+               4, 9, 17, 16, 0];
+
+console.log(arr.filter(element => element % 2=== 0));
---
+++ even2.js
@@ -0,0 +1,5 @@
+let arr = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
+
+for (let i=0; i < arr.length; i++) if (arr[i] % 2=== 0) {
+  console.log(arr[i]);
+}
