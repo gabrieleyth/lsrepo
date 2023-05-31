22cecf1a2dd24ecc9a3820ccb806a1efde5b105c
gabrieleyth <gabriel.leyth@outlook.com>
2023-05-31 09:14:49 +0000

    extension of test program

---
+++ obja.js
@@ -0,0 +1,8 @@
+let person = {
+  name:       'Bob',
+  occupation: 'web developer',
+  hobbies:    'painting',
+};
+
+console.log(person['name']); // name without quotes gives error
+console.log(person[name]);
